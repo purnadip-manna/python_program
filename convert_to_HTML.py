2fc42e191c1af534575d857856d7cd527757c61f
purnadip-manna <66995263+purnadip-manna@users.noreply.github.com>
2020-08-23 20:08:14 +0530

    Update convert_to_HTML.py

--- convert_to_HTML.py
+++ convert_to_HTML.py
@@ -3,8 +3,8 @@ fn = input("Enter python file name to read: ")
 fd = input("Enter file to write: ")
 space = ""
 
-filename = r"file_location" + fn
-filedump = r"file_location" + fd
+filename = r"file_location" + fn  # file_location -> of the main or source file
+filedump = r"file_location" + fd  # file_location -> of the new or destination file
 
 pyfile = open(filename, 'r')
 s = pyfile.read()
